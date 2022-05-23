22ac63d452b95fbd02a6106af838facb1d4ce42f
Zniker <kirillzolotarev111@mail.ru>
2022-05-24 02:57:38 +0300

    first commit

---
+++ _build/solver_application/CMakeFiles/solve.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/frox/lab09/solver_application/equation.cpp" "/home/frox/lab09/_build/solver_application/CMakeFiles/solve.dir/equation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../solver_application/../formatter_ex_lib"
+  "../solver_application/../solver_lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/frox/lab09/_build/formatter_ex_lib/CMakeFiles/formatter_ex.dir/DependInfo.cmake"
+  "/home/frox/lab09/_build/solver_lib/CMakeFiles/solver.dir/DependInfo.cmake"
+  "/home/frox/lab09/_build/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
